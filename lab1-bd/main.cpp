8e49fc7076c0f0269e06e19a3629ffca95959b05
Julio Arrieta <jaservice@vera.com.uy>
2018-10-17 00:34:49 -0300

    Se limpio el codigo y se esta trabajando con array para verificar las sentencias recibidas

--- lab1-bd/main.cpp
+++ lab1-bd/main.cpp
@@ -8,12 +8,6 @@ using namespace std;
 enum _retorno{OK, ERROR, NO_IMPLEMENTADA};
 typedef _retorno TipoRet;
 
-struct nodoListaParamentro{
-    string info;
-    nodoListaParamentro * sig;
-};
-typedef nodoListaParamentro * Parametro;
-
 struct nodoListaCelda{
     int nroCelda;
     string info;
@@ -48,8 +42,7 @@ typedef nodoListaTabla * Tabla;
 void readInput( string comando ); //Interpreta el comando de entrada
 void printHelp(); // Imprime la Ayuda con los comandos validos
 bool addTabla( Tabla &T, string nombreTabla );
-Parametro getParametros(string argumentos, int n);
-
+string getParametros(string allArg, int n);
 
 int main(){
     Tabla T = new nodoListaTabla; //dummy
@@ -71,101 +64,104 @@ void printHelp(){
     cout << "Nota: todos los comandos son 'case sensitive' "<<endl;
     cout << "  help  _______________________________________________________________________* IMPRIME LA AYUDA EN PANTALLA *" <<endl<<endl;
     cout << "  createTable(nombreTabla) ____________________________________________________* CREA UNA NUEVA TABLA *"<<endl<<endl;
-    cout << "\t\tEjemplo:\t\tcreateTable(\"Empleados\")"<<endl<<endl;
+    cout << "\t\tEjemplo:\t\tcreateTable(Empleados)"<<endl<<endl;
     cout << "  dropTable(nombreTabla) ______________________________________________________* ELIMINA UNA TABLA EXISTENTE *" <<endl<<endl;
-    cout << "\t\tEjemplo:\t\tdropTable(\"Empleados\")"<<endl<<endl;
+    cout << "\t\tEjemplo:\t\tdropTable(Productos)"<<endl<<endl;
     cout << "  addCol(nombreTabla, nombreCol) ______________________________________________* AGREGA UNA NUEVA COLUMNA A LA TABLA EXISTENTE *" <<endl<<endl;
-    cout << "\t\tEjemplo:\t\taddCol(\"Empleados\",\"Apellido\")"<<endl<<endl;
+    cout << "\t\tEjemplo:\t\taddCol(Empleados, Apellido)"<<endl<<endl;
     cout << "  dropCol(nombreTabla, nombreCol) _____________________________________________* ELIMINA UNA COLUMNA DE UNA TABLA *" <<endl<<endl;
-    cout << "\t\tEjemplo:\t\tdropCol(\"Proyectos\", \"idProyecto\")"<<endl<<endl;
+    cout << "\t\tEjemplo:\t\tdropCol(Proyectos, idProyecto)"<<endl<<endl;
     cout << "  insertInto(nombreTabla, valoresTupla) _______________________________________* INSERTA UN NUEVO REGISTRO EN LA TABLA *" <<endl<<endl;
-    cout << "\t\tEjemplo:\t\tinsertInto(\"Personas\", \"3333111\":\"Telma\":\"Perez\")"<<endl<<endl;
+    cout << "\t\tEjemplo:\t\tinsertInto(Personas, 3333111: Telma: Perez)"<<endl<<endl;
     cout << "  deleteFrom(nombreTabla, condicionEliminar) __________________________________* ELIMINA UN REGISTRO DE UNA TABLA *" <<endl<<endl;
-    cout << "\t\tEjemplo:\t\tdeleteFrom(\"Personas\", \"Perez\")"<<endl<<endl;
+    cout << "\t\tEjemplo:\t\tdeleteFrom(Personas, Perez)"<<endl<<endl;
     cout << "  update(nombreTabla, condicionModificar, columnaModificar, valorModificar) ___* ACTUALIZA UN CAMPO DE UNA TABLA *" <<endl<<endl;
-    cout << "\t\tEjemplo:\t\tupdate(\"Personas\", \"Nombre=Pepe\":\"CI\":\"1555000\")"<<endl<<endl;
-    cout << "  printDataTable(nombreTabla) _________________________________________________* IMPRIME TODOS LOS REGISTROS DE UNA TABLA *" <<endl<<endl;
-    cout << "\t\tEjemplo:\t\tprintDataTable(\"Clientes\")"<<endl<<endl;
+    cout << "\t\tEjemplo:\t\tupdate(Personas, Nombre=Pepe: CI: 1555000)"<<endl<<endl;
+    cout << "  printDataTable(Clientes) _________________________________________________* IMPRIME TODOS LOS REGISTROS DE UNA TABLA *" <<endl<<endl;
+    cout << "\t\tEjemplo:\t\tprintDataTable(Clientes)"<<endl<<endl;
 }
 
+
+/****************     LEE EL INGRESO DE LOS COMANDOS     ************************/
 void readInput(string comando){
-    string blanco = " ";
     string actual;
-    int nroComas = 0;
-    for (int i=0; i<comando.length(); ++i){ // Borro todos los espacios en blanco
+    int nroComas =0;
+    int nroArg=0;
+
+    for (int i=0; i<comando.length(); ++i){ // Borra todos los espacios en blanco y cuenta los parametros que trae
         actual = comando[i];
-        if( blanco == actual ){
-           // cout <<"posicion "<< i << " caracter " << actual << endl;
+        if( actual==" " ){
             comando.erase(i, 1);
             i--;
         }
-        if( actual == ",")
+        if( actual=="," ){
             nroComas++;
+        }
     }
+
     size_t posApertura = comando.find("(");      // posicion del parentesis de apertura
     string sentencia = comando.substr(0,posApertura); //setencia ingresada con espacios
     size_t posCierre = comando.find(")");        // posicion de parentesis de cierre
-    string argALL = comando.substr(posApertura+1 ,(posCierre - posApertura -1)); // obtiene el contenido de los argumentos
-    cout << "El comando ingresado es: " << sentencia <<endl;
-    cout << "Y los argumentos son: " << argALL << endl;
-    cout << "el largo del comando es: " << comando.length()<<endl;
+    string allArg = comando.substr(posApertura+1 ,(posCierre - posApertura -1)); // obtiene el contenido de los argumentos
 
-    if( !argALL.empty() ){
-        size_t fin = argALL.find(":");
-    }else{
-        cout << "NO HAY ARGUMENTOS" <<endl;
+    if( allArg.empty() || allArg == " " )
+        nroArg = 0;
+    else
+        nroArg = nroComas+1;
+
+    cout << "El comando ingresado es: " << sentencia <<endl;
+    cout << "Y los argumentos son: " << allArg << endl;
+    cout << "El largo del comando es: " << comando.length()<<endl;
+    cout << "Numero de argumentos: " << nroArg <<endl;
+
+
+    if( sentencia=="createTable" && nroArg==1 ){ //createTable( nombreTabla)
+
     }
 
-    Parametro LParam;
-    LParam = NULL;
-    LParam = getParametros(argALL, nroComas);//devuelve una lista con los parametros
-    cout << LParam->sig->info <<endl<<endl;
+    if( sentencia=="dropTable" && nroArg==1 ){ // dropTable( nombreTabla )
 
-    if( sentencia == "createTable" ) //createTable("nombreTabla")
-        cout << "Operacion: createTable(\"nombreTabla\")"<< endl;
+    }
 
-    if( sentencia == "dropTable" ) // dropTable("nombreTabla");
-        cout << "Operacion: dropTable(\"nombreTabla\")" <<endl;
-    if( sentencia == "addCol" ) //addCol("nombreTabla","nombreCol")
-        cout << "Operacion: addCol(\"nombreTabla\",\"nombreCol\")"<<endl;
-    if( sentencia == "dropCol" )//   dropCol("nombreTabla","nombreCol")
-        cout << "Operacion: dropCol(\"nombreTabla\",\"nombreCol\")"<<endl;
-    if( sentencia == "insertInto" )// insertInto("nombreTabla","valoresTupla")
-        cout << "Operacion: insertInto(\"nombreTabla\",\"valoresTupla\")" <<endl;
-    if( sentencia == "deleteFrom") //deleteFrom("nombreTabla", "condicionEliminar")
-        cout << "Operacion: deleteFrom(\"nombreTabla\", \"condicionEliminar\")" <<endl;
-    if( sentencia == "update" ) // update("nombreTabla", "condicionModificar", "columnaModificar", "valorModificar")
-        cout << "Operacion: update(\"nombreTabla\", \"condicionModificar\", \"columnaModificar\", \"valorModificar\")"<<endl;
-    if( sentencia == "printDataTable" )// printDataTable( "nombreTabla");
-        cout << "Operacion: printDataTable( \"nombreTabla\")" <<endl;
-    if( sentencia == "help" )//  printHelp()
-        cout << "Operacion: printHelp()" << endl;
+    if( sentencia == "addCol" && nroArg==2 ) {//addCol( nombreTabla, nombreCol )
+
+    }
+
+    if( sentencia == "dropCol" && nroArg==2 ){ //   dropCol( nombreTabla, nombreCol)
+
+    }
+
+    if( sentencia == "insertInto" && nroArg>1 )// insertInto( nombreTabla,valoresTupla")
+
+    if( sentencia == "deleteFrom" && nroArg==1 ) //deleteFrom( nombreTabla, condicionEliminar )
+
+    if( sentencia == "update" && nroArg>3  ) // update( nombreTabla, condicionModificar, columnaModificar, valorModificar)
+
+    if( sentencia == "printDataTable" )// printDataTable( nombreTabla );
+
+    if( sentencia == "help" && nroArg==0  )//  printHelp()
+        printHelp();
     if(sentencia!="createTable" && sentencia!="dropTable" && sentencia!="addCol" && sentencia!="dropCol" && sentencia!="insertInto" && sentencia!="deleteFrom" && sentencia!="update" && sentencia!="printDataTable" && sentencia!="help" )
         cout << "\t¡EL comando '" << comando <<"' no es valido!" << '\n';
 }
 
-Parametro getParametros(string argumentos, int n){
-    Parametro aux, res = new nodoListaParamentro;
-    res->sig = NULL;
-    aux = res;
-    int posicion= 0;
-
-    if( !argumentos.empty() && n==0 ){//Si no tiene coma pero no esta vacio hay un argumento
-        aux->sig = new nodoListaParamentro;
-        aux = aux->sig;
-        aus->sig = NULL;
-        aux->info = argumentos;
-    }
-    if( n > 0 ){
-        for(int i=0; i<=n; i++){
-            aux->sig = new nodoListaParamentro;
-            aux = aux->sig;
-            aux->info = argumentos.find()
+string getParametro(string allArg, int nroArg){
+    string c;
+    int nroComas;
+    if( !allArg.empty() ){ //Cuenta la cantidad y la posicion de las comas en los argumentos
+        for(int j=0; j< allArg.length(); j++ ){
+            c = allArg[j];
+            if( c == "," ){
+                nroComas++;
+            }
         }
+    }else{
+        cout << "NO HAY ARGUMENTOS" <<endl;
     }
 
-    return res;
 }
+
+
 
 bool addTabla( Tabla &T, string nombreTabla){
 
